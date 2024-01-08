8656e778513f44edb4cd6ac2eff97fb608dba44d
darbandi <darbandidr99@gmail.com>
2024-01-08 15:23:09 +0330

    added base templates set

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item"><strong>Dokoon</strong></router-link>
+      <a class="navbar-burger" aria-label="true" aria-expanded="false" data-target="navbar-menu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+      <div class="navbar-brand" id="navbar-menu" >
+        <div class="navbar-end"></div>
+      </div>
+    </div>
+    <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-end">
+        <router-link to="/summer" class="navbar-item" >Summer</router-link>
+        <router-link to="/winter" class="navbar-item" >Winter</router-link>
+        <div class="navbar-item">
+          <div class="button">
+            <router-link to="/log-in" class="button is-light">Log In</router-link>
+            <router-link to="/cart" class="button is-success">
+              <span class="icon" ><i class="fas fa-shopping-cart"></i></span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </nav>
-  <router-view/>
+  <section class="section">
+    <router-view/>
+  </section>
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '../node_modules/bulma';
 </style>
